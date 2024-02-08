14a62585fc8c2246a2c1e7f08b2e90cb8596291c
Neil Casas <neilalfonzcasas@gmail.com>
2024-02-09 02:03:24 +0800

    fixed js password validation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const password = document.querySelector('#pwd');
 const confirmPassword = document.querySelector('#cpwd');
 const form = document.querySelector('form');
+const inputFields = document.querySelectorAll('input[type="text"], input[type="email"], input[type="number"], input[type="password"]');
+const required = document.querySelectorAll('span');
 
 form.addEventListener('submit', (event) => {
-
     if (password.value !== confirmPassword.value) {
         alert('Passwords do not match');
     } else {
         alert('Login success');
     }
-})
+});
